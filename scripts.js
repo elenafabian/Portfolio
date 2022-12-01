a17f9cad268ed3a7c4eed190d63bf8a654830cd8
Elena Fabian <elenafabian@Elenas-MacBook-Pro.local>
2022-12-01 12:38:12 -0600

    trying submit button with ajax

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ function sendMessage() {
     confirmation.style.visibility = "visible";
     return;
 }
+
+function loadDoc(event) {
+    preventDefault(event);
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+      document.getElementsByClassName("contact-body-right2").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+
+    var confirmation = document.querySelector("#confirmation");
+    confirmation.style.visibility = "visible";
+    return;
+  }
