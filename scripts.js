b2e892b0699c719eb1e7cb81304bab5231702eca
Elena Fabian <elenafabian@Elenas-MacBook-Pro.local>
2022-12-01 14:43:46 -0600

    try3

--- scripts.js
+++ scripts.js
@@ -4,21 +4,21 @@ function sendMessage() {
     return;
 }
 
-$("#cpa-form").submit(function(e){
+$("#send-button").submit(function(e){
   e.preventDefault();
 });
 
-function loadDoc(event) {
+/* function loadDoc(event) {
     event.preventDefault();
 
-    /* const xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
       document.getElementsByClassName("contact-body-right2").innerHTML = this.responseText;
     }
     xhttp.open("GET", "ajax_info.txt", true);
-    xhttp.send(); */
+    xhttp.send();
 
     var confirmation = document.querySelector("#confirmation");
     confirmation.style.visibility = "visible";
     return;
-  }
+} */
