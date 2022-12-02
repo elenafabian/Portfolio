521f5712532d3c91ad7e784da1a92b3a7336c656
Elena Fabian <elenafabian@Elenas-MBP.hsd1.il.comcast.net>
2022-12-02 12:23:43 -0600

    data collection

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
-function sendMessage() {
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwaVa4C4kQmoONtjrHuzEfYps0c6x0fqwovo0E19jZl_nw10YkVjA7mFeIapw38aeLmUA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message));
+
+
+
     var confirmation = document.querySelector("#confirmation");
     confirmation.style.visibility = "visible";
-    return;
-}
-
-$('#form').submit(function (e) {
-    // custom handling here
-    e.preventDefault();
-});
+    var entries = document.querySelectorAll('.entry');
+    entries.forEach(function(currentValue, currentIndex, listObj){
+        currentValue.style.color = "grey";
+    })
+})
