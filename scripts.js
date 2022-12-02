4e907462fb8ff0b91b92333ab545df871120dad1
Elena Fabian <86376792+elenafabian@users.noreply.github.com>
2022-12-02 12:27:46 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+<!-- thanks to https://github.com/jamiewilson/form-to-google-sheets for the google sheet connection -->
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwaVa4C4kQmoONtjrHuzEfYps0c6x0fqwovo0E19jZl_nw10YkVjA7mFeIapw38aeLmUA/exec'
 const form = document.forms['submit-to-google-sheet']
 
@@ -7,8 +9,7 @@ form.addEventListener('submit', e => {
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message));
 
-
-
+    <!-- i added this -->
     var confirmation = document.querySelector("#confirmation");
     confirmation.style.visibility = "visible";
     var entries = document.querySelectorAll('.entry');
